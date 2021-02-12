cc637e626e6c6c576456e3ae7300f7133a9c80d6
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-12 12:25:59 +0100

    started on error handling

--- src/client.ts
+++ src/client.ts
@@ -16,16 +16,49 @@ export class HTTPClient {
         method: string,
         headers?: object,
     ) : Promise<unknown> => {
-        const resp = await this.FETCH(`https://api.stripe.com/v1${path}`, {
-            ...(method === 'POST' ? { body: qs.stringify(body) } : {}),
-            headers: {
-                Authorization: `Bearer ${this.STRIPE_SECRET_KEY}`,
-                'Content-type': 'application/x-www-form-urlencoded',
-                ...headers,
-            },
-            method,
-        })
-    
-        return await resp.json()
+        try {
+            const resp = await this.FETCH(`https://api.stripe.com/v1${path}`, {
+                ...(method === 'POST' ? { body: qs.stringify(body) } : {}),
+                headers: {
+                    Authorization: `Bearer ${this.STRIPE_SECRET_KEY}`,
+                    'Content-type': 'application/x-www-form-urlencoded',
+                    ...headers,
+                },
+                method,
+            })
+
+            const data = await resp.json()
+
+            if (resp.ok) {
+                return data
+            } else {
+                switch (resp.status) {
+                    case 400:
+                        break;
+                    case 401:
+                        break;
+                    case 402:
+                        break;
+                    case 403:
+                        break;
+                    case 404:
+                        break;
+                    case 409:
+                        break;
+                    case 429:
+                        break;
+                    case 500:
+                    case 502:
+                    case 503:
+                    case 504:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        } catch (error) {
+            throw error
+        }
     }
 }
