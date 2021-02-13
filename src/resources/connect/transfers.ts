3458fa541eb86b2d044916a69bcd7fc53e8aadf6
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-13 14:18:58 +0100

    created the transfers resource

---
+++ src/resources/connect/transfers.ts
@@ -0,0 +1,57 @@
+export namespace transfers {
+    export let client: Function
+
+    export function create(
+        params: {
+            amount: number,
+            currency: string,
+            destination: string,
+            description?: string,
+            metadata?: [string, unknown],
+            source_transaction?: unknown,
+            source_type?: unknown,
+            transfer_group?: unknown
+        }, stripeAccount?: string,
+    ) : Promise<unknown> {
+        return client(`/transfers`, params, 'POST', stripeAccount
+        ? { 'Stripe-Account': stripeAccount }
+        : {},)
+    }
+
+    export function retrieve(
+        id: string,
+        stripeAccount?: string,
+    ) : Promise<unknown> {
+        return client(`/transfers/${id}`, {}, 'GET', stripeAccount
+        ? { 'Stripe-Account': stripeAccount }
+        : {},)
+    }
+
+    export function update(
+        id: string,
+        params: {
+            description?: string,
+            metadata?: [string, unknown]
+        }, stripeAccount?: string,
+    ) : Promise<unknown> {
+        return client(`/transfers/${id}`, params, 'POST', stripeAccount
+        ? { 'Stripe-Account': stripeAccount }
+        : {},)
+    }
+
+    export function list(
+        params: {
+            destination?: string,
+            created?: object,
+            ending_before?: string,
+            limit?: number,
+            starting_after?: string,
+            transfer_group?: unknown
+        }, stripeAccount?: string,
+    ) : Promise<unknown> {
+        return client(`/topups?destination=${params.destination}&limit=${params.limit}&transfer_group=${params.transfer_group}&created=${params.created}&ending_before=${params.ending_before}&starting_after=${params.starting_after}`, {}, 'GET', stripeAccount
+        ? { 'Stripe-Account': stripeAccount }
+        : {},)
+    }
+
+}
