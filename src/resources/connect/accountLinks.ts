f6b85bb45962d4eb618d78c48bfe81101fc47f5e
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-13 13:15:42 +0100

    added accountLinks

---
+++ src/resources/connect/accountLinks.ts
@@ -0,0 +1,17 @@
+export namespace accountLinks {
+    export let client: Function
+
+    export function create(
+        params: {
+            account: string,
+            refresh_url: string,
+            return_url: string,
+            type: string,
+            collect?: string
+        }, stripeAccount?: string,
+    ) : Promise<unknown> {
+        return client('/account_links', params, 'POST', stripeAccount
+        ? { 'Stripe-Account': stripeAccount }
+        : {},)
+    }
+}
