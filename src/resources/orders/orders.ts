d7f73c3fc1390893ca17a24686282a17d573b510
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-15 20:16:18 +0100

    added the orders resource

---
+++ src/resources/orders/orders.ts
@@ -0,0 +1,108 @@
+export namespace orders {
+    export let client: Function
+
+    export function create(
+        params: {
+            currency: string,
+            customer?: string,
+            email?: string,
+            items?: object,
+            metadata?: [string, unknown],
+            shipping?: object,
+            coupon?: string
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            '/orders',
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function retrieve(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/orders/${id}`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function update(
+        id: string,
+        params: {
+            metadata?: [string, unknown],
+            shipping?: object,
+            status?: string,
+            coupon?: string,
+            selected_shipping_method?: unknown
+        }, stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/orders/${id}`,
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function pay(
+        id: string,
+        params: {
+            customer: string,
+            source: unknown,
+            email?: string,
+            metadata?: [string, unknown],
+            application_fee?: number
+        }, stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/orders/${id}`,
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function list(
+        params: {
+            customer?: string,
+            status?: string,
+            created?: number,
+            ending_before?: string,
+            ids?: string[],
+            limit?: number,
+            starting_after?: string,
+            status_transitions?: object,
+            upstream_ids?: string[]
+        }, stripeAccount?: string,
+    ): Promise<unknown> {
+        throw Error('not implemented')
+        /* return client(
+            `/orders?`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        ) */
+    }
+
+    export function returnOrder(
+        id: string,
+        params: {
+            items?: object
+        }, stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/orders/${id}/returns`,
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+}
