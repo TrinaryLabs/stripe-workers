f9e98fc3c23cc6e01b9673831a960eee61cc68c7
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-15 14:06:03 +0100

    added the coupons resource

---
+++ src/resources/billing/coupons.ts
@@ -0,0 +1,69 @@
+export namespace coupons {
+    export let client: Function
+
+    export function create(
+        params: {
+            duration: string,
+            amount_off?: number,
+            currency?: string,
+            duration_in_months?: number,
+            metadata?: [string, unknown],
+            name?: string,
+            percent_off?: number,
+            id?: string,
+            applies_to?: object,
+            max_redemtions?: number,
+            redeem_by?: number
+        }, stripeAccount?: string) : Promise<unknown> {
+        return client(
+            '/coupons',
+            params,
+            'POST', stripeAccount
+            ? { 'Stripe-Account': stripeAccount }
+            : {},
+        )
+    }
+
+    export function update(
+        id: string,
+        params: {
+            metadata?: [string, unknown],
+            name?: string
+        }, stripeAccount?: string) : Promise<unknown> {
+        return client(
+            `/coupons/${id}`,
+            params,
+            'POST', stripeAccount
+            ? { 'Stripe-Account': stripeAccount }
+            : {},
+        )
+    }
+
+    export function del(
+        id: string,
+        stripeAccount?: string) : Promise<unknown> {
+        return client(
+            `/coupons/${id}`,
+            {},
+            'DELETE', stripeAccount
+            ? { 'Stripe-Account': stripeAccount }
+            : {},
+        )
+    }
+
+    export function list(
+        params: {
+            created?: string,
+            ending_before?: string,
+            limit?: number,
+            starting_after?: string,
+        }, stripeAccount?: string) : Promise<unknown> {
+        return client(
+            `/coupons?limit=${params.limit}&created=${params.created}&ending_before=${params.ending_before}&starting_after=${params.starting_after}`,
+            {},
+            'GET', stripeAccount
+            ? { 'Stripe-Account': stripeAccount }
+            : {},
+        )
+    }
+}
