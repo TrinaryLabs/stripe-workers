8a98113d4d0d1296af126145364daf6bd3528f09
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-15 19:43:07 +0100

    added the sigma resource

---
+++ src/resources/sigma/scheduledQueryRuns.ts
@@ -0,0 +1,34 @@
+export namespace sigma {
+    export namespace scheduledQueryRuns {
+        export let client: Function
+
+        export function retrieve(
+            id: string,
+            stripeAccount?: string
+        ) : Promise<unknown> {
+            return client(
+                `/sigma/scheduled_query_runs/${id}`,
+                {},
+                'GET', stripeAccount
+                ? { 'Stripe-Account': stripeAccount }
+                : {},
+            )
+        }
+
+        export function list(
+            params: {
+                ending_before?: string,
+                limit?: number,
+                starting_after?: string
+            }, stripeAccount?: string
+        ) : Promise<unknown> {
+            return client(
+                `/sigma/scheduled_query_runs?limit=${params.limit}&ending_before=${params.ending_before}&starting_after=${params.starting_after}`,
+                {},
+                'GET', stripeAccount
+                ? { 'Stripe-Account': stripeAccount }
+                : {},
+            )
+        }
+    }
+}
