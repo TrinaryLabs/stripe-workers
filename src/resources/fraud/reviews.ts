b2f8d90cdd22bbad19abb9d2b5ca288a88c3579f
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-17 19:11:23 +0100

    added reviews API

---
+++ src/resources/fraud/reviews.ts
@@ -0,0 +1,46 @@
+import qs from 'qs'
+
+export namespace reviews {
+    export let client: Function
+
+    export function approve(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/reviews/${id}/approve`,
+            {},
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function retrieve(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/reviews/${id}`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function list(
+        params: {
+            created?: object,
+            ending_before?: string,
+            limit?: number,
+            starting_after?: string
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/reviews?${qs.stringify(params)}`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+}
