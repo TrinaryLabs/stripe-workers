b2be7dd02ffac8bdd3c0b8b22d65ff6b9ce2d599
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-19 19:35:16 +0100

    added the balance API resource

---
+++ src/resources/balance.ts
@@ -0,0 +1,14 @@
+export namespace balance {
+    export let client: Function
+
+    export function retrieve(
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            '/balance',
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+}
