8b6c7cf3907dc1a276043294aaa3b8b0d8587034
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-20 20:10:44 +0100

    added the payouts API

---
+++ src/resources/payouts.ts
@@ -0,0 +1,100 @@
+import qs from 'qs'
+
+export namespace payouts {
+    export let client: Function
+
+    export function create(
+        params: {
+            amount: number,
+            currency: string,
+            description?: string,
+            metadata?: [string, unknown],
+            statement_descriptor?: string,
+            destination?: string,
+            method?: string,
+            source_type?: string
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            '/payouts',
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function retrieve(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/payouts/${id}`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function update(
+        id: string,
+        params: {
+            metadata?: [string, unknown]
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/payouts/${id}`,
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function list(
+        params: {
+            status?: string,
+            arrival_date?: object,
+            created?: object
+            destination?: string,
+            ending_before?: string
+            limit?: number
+            starting_after?: string
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/payouts?${qs.stringify(params)}`,
+            {},
+            'GET',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function cancel(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/payouts/${id}/cancel`,
+            {},
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+
+    export function reverse(
+        id: string,
+        params: {
+            metadata?: [string, unknown]
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/payouts/${id}/reverse`,
+            params,
+            'POST',
+            stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+        )
+    }
+}
