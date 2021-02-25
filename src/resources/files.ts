60b269cba146281e55f1f0c8b7c6ce67377c16a9
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-02-25 15:26:22 +0100

    added the files resource

---
+++ src/resources/files.ts
@@ -0,0 +1,60 @@
+import qs from 'qs'
+
+export namespace files {
+    export let client: Function
+
+    export function create(
+        params: {
+            file: unknown,
+            purpose: string,
+            file_link_data?: object
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            '/files',
+            params,
+            'POST',
+            {
+                headers: stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+                host: 'https://files.stripe.com/v1'
+            }
+        )
+    }
+
+    export function retrieve(
+        id: string,
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/files/${id}`,
+            {},
+            'GET',
+            {
+                headers: stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+                host: 'https://files.stripe.com/v1'
+            }
+        )
+    }
+
+    export function list(
+        params: {
+            purpose?: string
+            created?: object
+            ending_before?: string
+            limit?: number
+            starting_after?: string
+        },
+        stripeAccount?: string,
+    ): Promise<unknown> {
+        return client(
+            `/files?${qs.stringify(params)}`,
+            {},
+            'GET',
+            {
+                headers: stripeAccount ? { 'Stripe-Account': stripeAccount } : {},
+                host: 'https://files.stripe.com/v1'
+            }
+        )
+    }
+}
