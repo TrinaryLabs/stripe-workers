f84426ae1bca61f33ce7d28f26849d0a9e839d95
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-03-23 13:57:12 +0100

    updated to fix issue with types not found

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,65 @@
+export * from '../dist/types/src/resources/billing/billingPortal'
+export * from '../dist/types/src/resources/billing/coupons'
+export * from '../dist/types/src/resources/billing/creditNotes'
+export * from '../dist/types/src/resources/billing/invoiceItems'
+export * from '../dist/types/src/resources/billing/invoices'
+export * from '../dist/types/src/resources/billing/promotionCodes'
+export * from '../dist/types/src/resources/billing/subscriptionItems'
+export * from '../dist/types/src/resources/billing/subscriptionSchedules'
+export * from '../dist/types/src/resources/billing/subscriptions'
+export * from '../dist/types/src/resources/billing/taxRates'
+export * from '../dist/types/src/resources/billing/plans'
+
+export * from '../dist/types/src/resources/checkout/sessions'
+
+export * from '../dist/types/src/resources/connect/accounts'
+export * from '../dist/types/src/resources/connect/accountLinks'
+export * from '../dist/types/src/resources/connect/applicationFees'
+export * from '../dist/types/src/resources/connect/countrySpecs'
+export * from '../dist/types/src/resources/connect/transfers'
+
+export * from '../dist/types/src/resources/paymentMethods/paymentMethods'
+export * from '../dist/types/src/resources/paymentMethods/sources'
+
+export * from '../dist/types/src/resources/reporting/report'
+
+export * from '../dist/types/src/resources/sigma/scheduledQueryRuns'
+
+export * from '../dist/types/src/resources/webhooks/webhookEndpoints'
+
+export * from '../dist/types/src/resources/orders/orderReturns'
+export * from '../dist/types/src/resources/orders/orders'
+export * from '../dist/types/src/resources/orders/skus'
+
+export * from '../dist/types/src/resources/issuing/issuing'
+
+export * from '../dist/types/src/resources/balance'
+export * from '../dist/types/src/resources/balanceTransactions'
+export * from '../dist/types/src/resources/charges'
+export * from '../dist/types/src/resources/disputes'
+export * from '../dist/types/src/resources/events'
+export * from '../dist/types/src/resources/fileLinks'
+export * from '../dist/types/src/resources/mandates'
+export * from '../dist/types/src/resources/topups'
+export * from '../dist/types/src/resources/payouts'
+export * from '../dist/types/src/resources/prices'
+export * from '../dist/types/src/resources/products'
+export * from '../dist/types/src/resources/refunds'
+export * from '../dist/types/src/resources/setupAttempts'
+export * from '../dist/types/src/resources/setupIntents'
+export * from '../dist/types/src/resources/tokens'
+
+export * from '../dist/types/src/resources/terminal/terminal'
+
+export * from '../dist/types/src/resources/fraud/radar'
+export * from '../dist/types/src/resources/fraud/reviews'
+
+export * from '../dist/types/src/resources/files'
+
+export * from '../dist/types/src/resources/paymentIntents'
+export * from '../dist/types/src/resources/customers'
+export * from '../dist/types/src/error'
+export * from '../dist/types/src/client'
+export * from '../dist/types/src/index'
+
 declare module 'stripe-workers'
