c6c23a12bb7b03f2084c85f454a453287c297006
EduM22 <38257387+EduM22@users.noreply.github.com>
2021-04-10 22:57:15 +0200

    added small tests & some clean up/fix

---
+++ test/resources/billing/billingPortal.spec.ts
@@ -0,0 +1,94 @@
+import { expect } from 'chai'
+import { stripe } from '../../_setup'
+
+/*
+describe('BillingPortal', async () => {
+    describe('Sessions Resource', async () => {
+        describe('create', async () => {
+            it('Sends the correct request', async () => {
+                const params = {
+                    customer: 'cus_123',
+                    return_url: 'https://stripe.com/return'
+                }
+                let data = await stripe.billingPortal.sessions.create(params.customer, params.return_url)
+
+                expect(data).to.deep.equal({
+                    method: 'POST',
+                    url: '/v1/billing_portal/sessions',
+                    headers: {},
+                    data: params,
+                    settings: {},
+                })
+            })
+        })
+    })
+
+})
+*/
+/*
+describe('Configurations Resource', () => {
+    describe('create', () => {
+        it('Sends the correct request', () => {
+            const params = {
+                business_profile: {
+                    privacy_policy_url: 'https://example.com/privacy',
+                    terms_of_service_url: 'https://example.com/tos',
+                },
+                features: {
+                    customer_update: {
+                        allowed_updates: ['address'],
+                        enabled: true,
+                    },
+                },
+            }
+            stripe.billingPortal.configurations.create(params)
+            expect(stripe.LAST_REQUEST).to.deep.equal({
+                method: 'POST',
+                url: '/v1/billing_portal/configurations',
+                headers: {},
+                data: params,
+                settings: {},
+            })
+        })
+    })
+    describe('update', () => {
+        it('Sends the correct request', () => {
+            const params = {
+                active: false,
+            }
+            stripe.billingPortal.configurations.update('bpc_123', params)
+            expect(stripe.LAST_REQUEST).to.deep.equal({
+                method: 'POST',
+                url: '/v1/billing_portal/configurations/bpc_123',
+                headers: {},
+                data: params,
+                settings: {},
+            })
+        })
+    })
+    describe('retrieve', () => {
+        it('Sends the correct request', () => {
+            stripe.billingPortal.configurations.retrieve('bpc_123')
+            expect(stripe.LAST_REQUEST).to.deep.equal({
+                method: 'GET',
+                url: '/v1/billing_portal/configurations/bpc_123',
+                headers: {},
+                data: {},
+                settings: {},
+            })
+        })
+    })
+    describe('list', () => {
+        it('Sends the correct request', () => {
+            stripe.billingPortal.configurations.list()
+            expect(stripe.LAST_REQUEST).to.deep.equal({
+                method: 'GET',
+                url: '/v1/billing_portal/configurations',
+                headers: {},
+                data: {},
+                settings: {},
+            })
+        })
+    })
+})
+*/
